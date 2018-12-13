90b861cf72b38cca4d210f97e93fc5b54516d041
Peter Stein <peterstein@protonmail.com>
2018-12-13 20:05:00 +0100

    Improve tests

--- tests/test_real_data.py
+++ tests/test_real_data.py
@@ -7,24 +7,20 @@ from cernrequests.cookies import get_sso_cookies
 
 
 def test_dqmgui():
+    """
+    The DQM GUI does not require cookies, but does require Grid User Certificates
+    :return:
+    """
     url = "https://cmsweb.cern.ch/dqm/offline/jsonfairy/archive/321012/StreamExpress/Run2018D-Express-v1/DQMIO/"
     expected = json.loads('{"hist": "unsupported type"}')
 
     assert expected == cernrequests.get(url).json()
 
 
-"""
-def test_get_cookie():
-    url = "https://lumis.web.cern.ch/api/all/?run_min=323755&run_max=323755"
-
-    cert, key = cernrequests.core.default_user_certificate_paths()
-    cookie = cern_sso.cert_sign_on(url, cert, key)
-
-    # requests.get(url, cookies=cookie)
-"""
-
-
 def test_lumis():
+    """
+    The lumis website does not require the CERN Root Certificate Authority
+    """
     url = "https://lumis.web.cern.ch/api/all/?run_min=323755&run_max=323755"
     expected = json.loads(
         '{"323755": [[27, 815], [816, 817], [818, 823], '
@@ -39,7 +35,12 @@ def test_lumis():
 
 
 def test_wbm():
-    url = "https://cmswbm.cern.ch/cmsdb/servlet/LhcMonitor?FORMAT=XML"
+    """
+    The CMS WBM webiste requires the CERN Root Certificate Authority
+    """
+    url = "https://cmswbm.cern.ch/cmsdb/servlet/RunSummary?RUN=211831&FORMAT=XML"
     cookies = get_sso_cookies(url)
     response = cernrequests.get(url, cookies=cookies)
-    print(response.content)
+
+    expected = "<nLumiSections>160</nLumiSections>"
+    assert expected in response.content
