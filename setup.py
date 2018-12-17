d3ef413ebcb3caf90e5a462ee6815df01af76b6d
Peter Stein <peterstein@protonmail.com>
2018-12-17 18:59:00 +0100

    Add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="cernrequests",
     version="0.1",
     desription="CERN wrapper around the requests package",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/ptrstn/cernrequests",
     author="Peter Stein",
     author_email="peter.stein@cern.ch",
